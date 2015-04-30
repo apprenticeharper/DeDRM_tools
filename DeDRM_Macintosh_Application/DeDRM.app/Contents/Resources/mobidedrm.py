7ac8100ed4b09d81a88b2a57fd016ba8173bd15a
matimatik <matimatik@gmail.com>
2015-04-30 01:24:00 -0500

    Added a code to remove Kindle watermark. https://github.com/matimatik/DeDRM_tools/commit/f3fbc3573eadc4b505ed296176f35cd5a917cdbf

--- DeDRM_Macintosh_Application/DeDRM.app/Contents/Resources/mobidedrm.py
+++ DeDRM_Macintosh_Application/DeDRM.app/Contents/Resources/mobidedrm.py
@@ -317,6 +317,9 @@ class MobiBook:
                     elif type == 404 and size == 9:
                         # make sure text to speech is enabled
                         self.patchSection(0, '\0', 16 + self.mobi_length + pos + 8)
+                    elif type == 208 and size == 219:
+                        # remove watermark (atv:kin: stuff)
+                        self.patchSection(0, '\0'*211, 16 + self.mobi_length + pos + 8)
                     # print type, size, content, content.encode('hex')
                     pos += size
         except:
