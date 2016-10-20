d62b6c3831c11de7bcdeda90642c87e6cc605cc3
Jeffrey C <spaz926@users.noreply.github.com>
2016-10-19 22:52:23 -0500

    Fixed syntax, removed unnecessary whitespace

--- Other_Tools/B_and_N_Download_Helper/BN-Dload.user.js
+++ Other_Tools/B_and_N_Download_Helper/BN-Dload.user.js
@@ -7,14 +7,14 @@
 // ==/UserScript==
 
 function doIt() {
-	if ($('#adl1').length == 0) {
+	if ($('#adl1').length === 0) {
 		$('[action$="deleteItem"]').each(function(index) {
-			if ($(this).parent().find('[action$="EDSDeliverItem.aspx"]').length == 0) {
+			if ($(this).parent().find('[action$="EDSDeliverItem.aspx"]').length === 0) {
 				var delid = $(this).find('input').attr('value');
 				$(this).after('<span class="vb2"></span><form id="adl' + index + '" action="https://edelivery.barnesandnoble.com/EDS/EDSDeliverItem.aspx" class="download"><input value="' + delid + '" type="hidden" name="delid"><input type="hidden" value="Browser" name="clienttype"><input type="hidden" value="browser" name="deviceinfo"><button class="download "name="download">Alternative Download</button></form>');
 			}
 		});
-		
+
 	}
 
 	setTimeout (function() {
@@ -23,4 +23,4 @@ function doIt() {
 }
 
 doIt();
-	
+
