e4413613a5bab7f985f072b212ee13982bcd205c
kubik147 <kubik147@mail.com>
2020-08-01 00:14:25 +0200

    Simplify

--- DeDRM_plugin/adobekey.py
+++ DeDRM_plugin/adobekey.py
@@ -123,8 +123,6 @@ def unicode_argv():
         return [u"adobekey.py"]
     else:
         argvencoding = sys.stdin.encoding or "utf-8"
-        if argvencoding == None:
-            argvencoding = "utf-8"
         return [arg if isinstance(arg, six.text_type) else six.text_type(arg, argvencoding) for arg in sys.argv]
 
 class ADEPTError(Exception):
